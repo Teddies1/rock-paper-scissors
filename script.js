05db5f2960d78df5a12af527e832c9ead4eb8bb9
Teddies1 <nghonjoo@gmail.com>
2024-04-05 00:35:55 +0800

    implement computer choice function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function getComputerChoice() {
-    
+    let array = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random() * array.length); 
+    return array[randomIndex];
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+}
+  
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
