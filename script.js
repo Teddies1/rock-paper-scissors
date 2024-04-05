4a4a364603c6711b5a143768bda44e32698fe806
Teddies1 <nghonjoo@gmail.com>
2024-04-06 00:42:44 +0800

    finished implementing game

--- script.js
+++ script.js
@@ -6,9 +6,72 @@ function getComputerChoice() {
 
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    if (playerSelection.toLowerCase() === "rock"){
+        if (computerSelection === "rock"){
+            return 0
+        }
+        else if (computerSelection === "scissors"){
+            return 1
+        }
+        else{
+            return -1
+        }
+    }
+    else if (playerSelection.toLowerCase() === "scissors"){
+        if (computerSelection === "scissors"){
+            return 0
+        }
+        else if (computerSelection === "rock"){
+            return -1
+        }
+        else{
+            return 1
+        }
+    }
+    else if (playerSelection.toLowerCase() === "paper"){
+        if (computerSelection === "paper"){
+            return 0
+        }
+        else if (computerSelection === "scissors"){
+            return -1
+        }
+        else{
+            return 1
+        }
+    }
 }
-  
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
+
+function playGame(){  
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++){
+        let playerChoice = prompt("Choose either rock, paper or scissors!");
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerChoice, computerSelection);
+
+        if (result === 1){
+            console.log("Player wins this round!");
+            playerScore++;
+        }
+        else if (result === 0){
+            console.log("This round is a tie!");
+        }
+        else{
+            console.log("Player loses this round!");
+            computerScore++;;
+        }
+    }
+    
+    if (playerScore > computerScore){
+        return "Player wins!";
+    }
+    else if (computerScore > playerScore){
+        return "Player loses!";
+    }
+    else{
+        return "It's a draw!"
+    }
+}
+
+console.log(playGame());
